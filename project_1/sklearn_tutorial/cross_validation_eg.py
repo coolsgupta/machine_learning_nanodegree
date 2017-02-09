005b55a63747c159aa1e08c67fff7046d2c19bbe
= <=>
2017-02-09 23:19:37 +0530

    added cross validation eg

---
+++ project_1/sklearn_tutorial/cross_validation_eg.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python
+
+""" this example borrows heavily from the example
+    shown on the sklearn documentation:
+
+    http://scikit-learn.org/0.17/modules/cross_validation.html
+
+"""
+from sklearn.cross_validation import train_test_split
+from sklearn import datasets
+from sklearn.svm import SVC
+import numpy as np
+
+iris = datasets.load_iris()
+features = iris.data
+labels = iris.target
+
+###############################################################
+### YOUR CODE HERE
+###############################################################
+
+### import the relevant code and make your train/test split
+### name the output datasets features_train, features_test,
+### labels_train, and labels_test
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size = 0.4, random_state = 0)
+### set the random_state to 0 and the test_size to 0.4 so
+### we can exactly check your result
+
+
+
+
+###############################################################
+
+clf = SVC(kernel="linear", C=1.)
+clf.fit(features_train, labels_train)
+
+print clf.score(features_test, labels_test)
+
+
+##############################################################
+def submitAcc():
+    return clf.score(features_test, labels_test)
