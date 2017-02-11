ce75f099bec91428be3b46da7f6e69270696ac23
= <=>
2017-02-11 22:00:58 +0530

    mean absolute error

---
+++ project_1/evaluation_metrics/mean_absolute_error.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+# Load the dataset
+from sklearn.datasets import load_linnerud
+
+linnerud_data = load_linnerud()
+X = linnerud_data.data
+y = linnerud_data.target
+
+from sklearn.tree import DecisionTreeRegressor
+from sklearn.metrics import mean_absolute_error as mae
+from sklearn.linear_model import LinearRegression
+from sklearn.cross_validation import train_test_split
+
+# TODO: split the data into training and testing sets,
+# using the standard settings for train_test_split.
+# Then, train and test the classifiers with your newly split data instead of X and y.
+x_train, x_test, y_train, y_test = train_test_split(X,y,test_size=0.4,random_state=0)
+reg1 = DecisionTreeRegressor()
+reg1.fit(x_train, y_train)
+print "Decision Tree mean absolute error: {:.2f}".format(mae(y_test,reg1.predict(x_test)))
+
+reg2 = LinearRegression()
+reg2.fit(x_train, y_train)
+print "Linear regression mean absolute error: {:.2f}".format(mae(y_test,reg2.predict(x_test)))
+
+results = {
+ "Linear Regression": 10.41,
+ "Decision Tree": 12.08
+}
