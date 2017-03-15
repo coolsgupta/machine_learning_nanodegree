d1442f6ffd172efb98dac589cd7d33687eedff24
= <cool.sgupta@gmail.com>
2017-03-15 21:06:36 +0530

    create GNB clf

--- project_2/naive bayes/creating_the_GNB_clf.py
+++ project_2/naive bayes/creating_the_GNB_clf.py
@@ -1,2 +1,17 @@
 #doc: http://scikit-learn.org/stable/modules/generated/sklearn.naive_bayes.GaussianNB.html
 
+from sklearn.naive_bayes import GaussianNB
+
+
+def classify(features_train, labels_train):
+    ### import the sklearn module for GaussianNB
+    ### create classifier
+    ### fit the classifier on the training features and labels
+    ### return the fit classifier
+
+
+    ### your code goes here!
+    clf = GaussianNB();
+    clf.fit(features_train, labels_train)
+    return clf
+
