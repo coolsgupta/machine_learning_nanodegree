916616ff6a96fb49ff353307363ccecb6b2ba31f
coolsgupta <cool.sgupta@gmail.com>
2017-08-22 16:59:15 +0530

    keras 3

--- Deep_Learning/Deep_neural_networks/keras_steps.py
+++ Deep_Learning/Deep_neural_networks/keras_steps.py
@@ -1,28 +1,36 @@
 #a simple model with a single hidden layer might look like this:
 
 import numpy as np
-    from keras.models import Sequential
-    from keras.layers.core import Dense, Activation
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation
 
     # X has shape (num_rows, num_cols), where the training data are stored
     # as row vectors
-    X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
+X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]], dtype=np.float32)
 
     # y must have an output vector for each input vector
-    y = np.array([[0], [0], [0], [1]], dtype=np.float32)
+y = np.array([[0], [0], [0], [1]], dtype=np.float32)
 
     # Create the Sequential model
-    model = Sequential()
+model = Sequential()
 
     # 1st Layer - Add an input layer of 32 nodes with the same input shape as
     # the training samples in X
-    model.add(Dense(32, input_dim=X.shape[1]))
+model.add(Dense(32, input_dim=X.shape[1]))
 
     # 2rd Layer - Add a softmax activation layer
-    model.add(Activation('softmax'))
+model.add(Activation('softmax'))
 
     # 4th Layer - Add a fully connected output layer
-    model.add(Dense(1))
+model.add(Dense(1))
 
-    # 5th Layer - Add a sigmoid activation layer
-    model.add(Activation('sigmoid'))
+# 5th Layer - Add a sigmoid activation layer
+model.add(Activation('sigmoid'))
+
+model.compile(loss="categorical_crossentropy", optimizer="adam", metrics = ["accuracy"])
+
+model.summary()
+
+model.fit(X, y, nb_epoch=1000, verbose=0)
+
+model.evaluate()
