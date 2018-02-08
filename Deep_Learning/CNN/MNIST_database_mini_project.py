d9b1d7b1c135a495965406f68a9dd670d09391de
coolsgupta <cool.sgupta@gmail.com>
2018-02-08 17:40:31 +0530

    built model

--- Deep_Learning/CNN/MNIST_database_mini_project.py
+++ Deep_Learning/CNN/MNIST_database_mini_project.py
@@ -22,3 +22,21 @@ y_test = np_utils.to_categorical(y_test,10)
 # print first ten (one-hot) training labels
 print('One-hot labels:')
 print(y_train[:10])
+
+#we create a CNN with 784 nodes in input layer because we have 784 entries in our input vector since we converted the 28x28 matrix to a single vector
+
+#creating the model
+from keras.models import Sequential
+from keras.layers import Dense,Dropout,Flatten
+
+model = Sequential()
+model.add(Flatten(input_shape=x_train.shape[1:]))
+model.add(Dense(512,activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(512,activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(10,activation='softmax'))
+
+#summarise the model
+model.summary()
+
